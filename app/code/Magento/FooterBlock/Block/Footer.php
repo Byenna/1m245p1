a9f8dfdf4b02d164202c08fef2dac02cfcda6655
Byenna <72694655+Byenna@users.noreply.github.com>
2022-11-03 20:46:32 +0100

    added Block

---
+++ app/code/Magento/FooterBlock/Block/Footer.php
@@ -0,0 +1,36 @@
+<?php
+namespace Magento\FooterBlock\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Cms\Block\Block as cmsBlock;
+use Magento\Framework\App\Request\Http as request;
+
+class Footer extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+
+    public $cms;
+     
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context, cmsBlock $cmsblock, request $request) {
+            $this->cmsblock = $cmsblock;
+            $this->_request = $request;
+            parent::__construct($context);
+        }
+        
+        public function getCmsBlockMore() 
+        {
+            $cmsblock = [];
+
+            if ($this->_request->getFullActionName() === 'cms_index_index') {
+                $cmsblock['route'] = 'home';
+                $cmsblock['block'] = $this->cmsblock->setBlockId('footer_offer_more')->toHtml();
+            }
+
+            return $cmsblock;
+        }
+    
+}
